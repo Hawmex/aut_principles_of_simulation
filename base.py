80461e0d0076dbbba5b33081a1cb2c3a6649e577
Hawmex <hamed.araab.02@gmail.com>
2024-03-27 23:27:16 +0330

    refactor

--- base.py
+++ base.py
@@ -1,3 +1,4 @@
+import random
 from typing import *
 
 
@@ -5,26 +6,12 @@ class SimEvent[T: SimController]:
     def __init__(self, interval: float) -> None:
         assert interval >= 0, "Negative interval"
 
-        self._due: float
-        self._controller: T
+        self.due: float
+        self.controller: T
 
-        self._interval = interval
+        self.interval = interval
 
-    @property
-    def interval(self) -> float:
-        return self._interval
-
-    @property
-    def due(self) -> float:
-        assert self._due is not None
-        return self._due
-
-    @property
-    def controller(self) -> T:
-        assert self._controller is not None
-        return self._controller
-
-    def dispatch(self) -> None:
+    def trigger(self) -> None:
         raise NotImplementedError()
 
 
@@ -33,55 +20,32 @@ class SimController:
         assert initialEvent.interval == 0, "Non-zero interval for the initial event"
         assert stop > 0, "Non-positive stop time"
 
-        initialEvent._due = 0
-        initialEvent._controller = self
+        initialEvent.due = 0
+        initialEvent.controller = self
 
-        self._events: List[SimEvent] = [initialEvent]
-        self._clock: float = 0
-        self._immediateEventIndex: int = 0
+        self.futureEvents: List[SimEvent] = [initialEvent]
+        self.clock: float = 0
 
-        self._stop = stop
+        self.stop = stop
 
-    @property
-    def pastEvents(self) -> List[SimEvent]:
-        return self._events[: self._immediateEventIndex]
+    def dispatchEvent(self, event: SimEvent) -> None:
+        event.due = self.clock + event.interval
+        event.controller = self
 
-    @property
-    def futureEvents(self) -> List[SimEvent]:
-        return self._events[self._immediateEventIndex :]
-
-    @property
-    def clock(self) -> float:
-        return self._clock
-
-    @property
-    def stop(self) -> float:
-        return self._stop
-
-    def enqueueEvent(self, event: SimEvent) -> None:
-        event._due = self.clock + event.interval
-        event._controller = self
-
-        if not self.pastEvents:
-            previousEvent = self.pastEvents[-1]
-
-            assert event.due > previousEvent.due, "Event's due is not in the future"
-
-        futureEvents = self.futureEvents
-
-        futureEvents.append(event)
-        futureEvents.sort(key=lambda event: event.due)
-
-        self._events = self.pastEvents + futureEvents
+        self.futureEvents.append(event)
+        self.futureEvents.sort(key=lambda event: event.due)
 
     def simulate(self) -> None:
-        while True:
-            immediateEvent = self.futureEvents[0]
+        while self.futureEvents[0].due <= self.stop:
+            upcomingEvent = self.futureEvents.pop(0)
 
-            self._immediateEventIndex += 1
-            self._clock = immediateEvent.due
+            self.clock = upcomingEvent.due
 
-            if self.clock > self.stop:
-                break
+            upcomingEvent.trigger()
 
-            immediateEvent.dispatch()
+
+class DistributionFunction:
+    @staticmethod
+    def uniform(min: float, max: float) -> float:
+        assert max > min, "max must be greater than min"
+        return random.random() * (max - min) + min
--- distribution_functions.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-def uniform(min: float, max: float) -> float:
-    return random.random() * (max - min) + min
